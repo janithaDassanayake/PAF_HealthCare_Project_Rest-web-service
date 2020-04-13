13aeed3f4e7e0a8f951b763ee6b319a48a089e96
Prabha97 <prabhashwari.lk@gmail.com>
2020-04-13 16:35:01 +0530

    implemented insert operation in com class

--- PAF_HealthCare/src/com/DepartmentService.java
+++ PAF_HealthCare/src/com/DepartmentService.java
@@ -7,7 +7,9 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType; 
 
 //For JSON 
-import com.google.gson.*; 
+import com.google.gson.*;
+
+import bean.DepartmentBean;
 
 //For XML 
 import org.jsoup.*; 
@@ -23,5 +25,39 @@ public class DepartmentService {
 	@Produces(MediaType.TEXT_HTML)  
 	public String readDepartments()  {   
 		return departmentObj.readDepartments();
+	}
+	
+//	@POST 
+//	@Path("/") 
+//	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+//	@Produces(MediaType.TEXT_PLAIN) 
+//	public String insertDepartments(@FormParam("Hospital_ID") String Hospital_ID,
+//							@FormParam("Department_Name") String Department_Name,
+//							@FormParam("Head") String Head,    
+//							@FormParam("Staff_Vacancies") String Staff_Vacancies)
+//	{  
+//		String output = departmentObj.insertDepartments(Hospital_ID, Department_Name, Head, Staff_Vacancies);  
+//		return output; 
+//	}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertDeps(String DepData) {
+		// Convert the input string to a JSON object
+		JsonObject depjosnObj = new JsonParser().parse(DepData).getAsJsonObject();
+	
+		DepartmentBean dep_Bean = new DepartmentBean();
+		
+		dep_Bean.setHospital_ID(depjosnObj.get("Hospital_ID").getAsInt());
+		dep_Bean.setDepartment_Name(depjosnObj.get("Department_Name").getAsString());
+		dep_Bean.setHead(depjosnObj.get("Head").getAsString());
+		dep_Bean.setStaff_Vacancies(depjosnObj.get("Staff_Vacancies").getAsInt());
+		
+		// Read the values from the JSON object
+		String output = departmentObj.insertDepartments(dep_Bean);
+		return output;
+
 	}
 }
--- PAF_HealthCare/src/com/HospitalService.java
+++ PAF_HealthCare/src/com/HospitalService.java
@@ -9,7 +9,6 @@ import javax.ws.rs.core.MediaType;
 //For JSON 
 import com.google.gson.*;
 
-import bean.AppoinmentTypeBean;
 import bean.HospitalBean;
 
 //For XML 
