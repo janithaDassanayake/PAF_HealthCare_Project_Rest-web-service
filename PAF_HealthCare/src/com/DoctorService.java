4dcafbafb587eca3e69872102eb443af0e5d39e8
chamathkaaa <uviniwijesinghe@gmail.com>
2020-04-13 17:14:15 +0530

    model.doctor

--- PAF_HealthCare/src/com/DoctorService.java
+++ PAF_HealthCare/src/com/DoctorService.java
@@ -1,5 +1,30 @@
 package com;
 
-public class DoctorService {
+import model.Doctor; 
 
-}
+//For REST Service 
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+
+//For JSON 
+import com.google.gson.*; 
+
+//For XML 
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+@Path("/Doctors") 
+public class DoctorService {  
+	
+	Doctor docObj = new Doctor(); 
+
+	@GET  
+	@Path("/")  
+	@Produces(MediaType.TEXT_HTML)  
+	public String readItems()  
+	{   
+		return docObj.readItems();  
+	} 
+	
+} 
