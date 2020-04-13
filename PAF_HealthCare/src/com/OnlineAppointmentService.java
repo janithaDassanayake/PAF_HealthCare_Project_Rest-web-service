4f47b2d3dbf6ccd9f21f62f545248583a5dccd4c
SaumyaBandara0722 <saumya19970722@gmail.com>
2020-04-13 12:04:08 +0530

    initial project created

---
+++ PAF_HealthCare/src/com/OnlineAppointmentService.java
@@ -0,0 +1,131 @@
+package com;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.sql.Date;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Timer;
+import model.OnlineAppointment;
+
+import java.sql.Date;
+import java.sql.Time;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+//for json
+import com.google.gson.*;
+@Path("/Appointment")
+
+public class OnlineAppointmentService {
+	OnlineAppointment appObj = new OnlineAppointment();
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readItems() {
+		return  appObj.readDetails();
+	}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertDetails(@FormParam("patientId") int PatientID,
+	 @FormParam("dueDate") String DueDate,
+	 @FormParam("scheduleId") int ScheduleId,
+	 @FormParam("status") boolean Status) {
+		
+	String result = null;
+	 
+	// ==================convert util time to sql time=====================
+			DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+			DateFormat formatTime = new SimpleDateFormat("HH:mm:ss");
+
+			try {
+				java.util.Date date = (java.util.Date) format.parse(DueDate);
+				java.sql.Date sDate = convertUtilToSql(date);
+
+				String output = appObj.insertDetails(PatientID, DueDate, ScheduleId);
+				
+				result=output;
+
+			} catch (ParseException e) {
+				e.printStackTrace();
+				result = e.toString();
+			}
+			return result;
+		}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateDetails(String details)
+	{
+	 String result = null;
+	//Convert the input string to a JSON object
+	 JsonObject detailsObject = new JsonParser().parse(details).getAsJsonObject();
+	//Read the values from the JSON object
+	 String AppointmentID = detailsObject.get("AppointmentID").getAsString();
+	 String PatientID = detailsObject.get("PatientID").getAsString();
+	 String DueDate = detailsObject.get("DueDate").getAsString();
+	 String ScheduleId = detailsObject.get("ScheduleId").getAsString();
+			 
+	 // ==================convert util time to sql time=====================
+			DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+			DateFormat formatTime = new SimpleDateFormat("HH:mm:ss");
+
+			try {
+				java.util.Date date = (java.util.Date) format.parse(DueDate);
+				java.sql.Date sDate = convertUtilToSql(date);
+
+				 String output = appObj.updateDetails(AppointmentID, PatientID, DueDate, ScheduleId);
+				
+				result=output;
+
+			} catch (ParseException e) {
+				e.printStackTrace();
+				result = e.toString();
+			}
+			return result;
+		}
+	// =================== return sql date and time======================
+			private static java.sql.Date convertUtilToSql(java.util.Date uDate) {
+				java.sql.Date sDate = new java.sql.Date(uDate.getTime());
+				return sDate;
+			}
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteDetails(String details)
+	{
+	//Convert the input string to an XML document
+	 Document doc = Jsoup.parse(details, "", Parser.xmlParser());
+
+	//Read the value from the element <itemID>
+	 String AppointmentID = doc.select("appointmentId").text();
+	 String output = appObj.deleteDetails(AppointmentID);
+	return output;
+	}
+}
---
+++ PAF_HealthCare/src/model/Schedule.java
@@ -0,0 +1,82 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Schedule {
+	public Connection connect() {
+		Connection con = null;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(
+					"jdbc:mysql://127.0.0.1:3306/healthcaredb?useTimezone=true&serverTimezone=UTC",
+					"root", "");
+			// For testing
+			System.out.print("Successfully connected");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return con;
+	}
+	
+	// View Doctor Schedule
+	//public int d_id;
+	
+	public String viewAllSchedule() {
+
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border=\"1\"><tr><th>Schedule Id</th>"
+					+ "<th>Appointment Specification</th>"
+					+ "<th>Doctor</th>"
+					+ "<th>Hospital</th>"
+					+ "<th>Date</th>" 
+					+ "<th>Start Time</th>" 
+					+ "<th>End Time</th></tr>";
+
+			String query = "select s.Schedule_id,t.Appointment_Type,d.dname,d.hname,s.Date,s.Start_Time,s.End_Time from doctor d join appointment_scheduling s on s.D_id=d.did join appointment_type t on t.appointment_Id=s.App_id order by t.Appointment_Type";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String schedule_id = rs.getString("Schedule_id");
+				String app_type = rs.getString("Appointment_Type");
+				String d_name = rs.getString("dname");
+				String h_name = rs.getString("hname");
+				String date = rs.getString("Date");
+				String start_time = rs.getString("Start_Time");
+				String end_time = rs.getString("End_Time");
+				
+				
+				// Add into the html table
+				output += "<tr><td>" + schedule_id + "</td>";
+				output += "<td>" + app_type + "</td>";
+				output += "<td>" + d_name + "</td>";
+				output += "<td>" + h_name + "</td>";
+				output += "<td>" + date + "</td>";
+				output += "<td>" + start_time + "</td>";
+				output += "<td>" + end_time + "</td></tr>";
+			}
+
+			con.close();
+			// Complete the html table
+			output += "</tr></table>";
+
+		} catch (Exception e) {
+			output = "Error while reading the Doctors Details.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+}
