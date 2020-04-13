650e3547fbee3763dfbc27186a285231f8283e99
Prabha97 <prabhashwari.lk@gmail.com>
2020-04-13 15:14:16 +0530

    created read operation inside department com class and created an object

--- PAF_HealthCare/src/com/DepartmentService.java
+++ PAF_HealthCare/src/com/DepartmentService.java
@@ -1,5 +1,20 @@
 package com;
 
-public class DepartmentService {
+import model.Department;
 
+//For REST Service 
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+
+//For JSON 
+import com.google.gson.*; 
+
+//For XML 
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+@Path("/Department") 
+public class DepartmentService {
+	Department departmentObj = new Department();
 }
