1e6e44f61815e45d3efc0daca622a3504b740f0a
Prabha97 <prabhashwari.lk@gmail.com>
2020-04-16 02:21:33 +0530

    department controller created

--- Hospital_Management_Service/src/bean/DepartmentBean.java
+++ Hospital_Management_Service/src/bean/DepartmentBean.java
@@ -2,4 +2,49 @@ package bean;
 
 public class DepartmentBean {
 
+	private int Department_ID;
+	private int Hospital_ID;
+	private String Department_Name;
+	private String Head;
+	private int Staff_Vacancies;
+	
+	public int getDepartment_ID() {
+		return Department_ID;
+	}
+	
+	public int getHospital_ID() {
+		return Hospital_ID;
+	}
+	
+	public String getDepartment_Name() {
+		return Department_Name;
+	}
+	
+	public String getHead() {
+		return Head;
+	}
+	
+	public int getStaff_Vacancies() {
+		return Staff_Vacancies;
+	}
+	
+	public void setDepartment_ID(int Department_ID) {
+		this.Department_ID = Department_ID;
+	}
+	
+	public void setHospital_ID(int Hospital_ID) {
+		this.Hospital_ID = Hospital_ID;
+	}
+	
+	public void setDepartment_Name(String Department_Name) {
+		this.Department_Name = Department_Name;
+	}
+	
+	public void setHead(String Head) {
+		this.Head = Head;
+	}
+	
+	public void setStaff_Vacancies(int Staff_Vacancies) {
+		this.Staff_Vacancies = Staff_Vacancies;
+	}
 }
