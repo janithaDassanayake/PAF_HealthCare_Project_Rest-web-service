7a1732cd3fe196bc7aafb14f5cba6e0303181ece
chamathkaaa <uviniwijesinghe@gmail.com>
2020-04-16 13:20:13 +0530

    dbConn, read, @GET created

--- Doctor_Management_Service/src/com/DoctorService.java
+++ Doctor_Management_Service/src/com/DoctorService.java
@@ -1,5 +1,29 @@
 package com;
 
+import model.Doctor;
+
+//For REST Service 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For JSON 
+import com.google.gson.*;
+
+//For XML 
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/DocServ")
 public class DoctorService {
 
+	Doctor itemObj = new Doctor();
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readItems() {
+		return "Hello";
+	}
+	
 }
