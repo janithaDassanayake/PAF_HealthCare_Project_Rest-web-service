c8816491b8f13e86f7801427b9ca795a571bae06
Prabha97 <prabhashwari.lk@gmail.com>
2020-04-17 03:21:52 +0530

    added modifications to dep bean

--- Hospital_Management_Service/src/bean/DepartmentBean.java
+++ Hospital_Management_Service/src/bean/DepartmentBean.java
@@ -8,6 +8,18 @@ public class DepartmentBean {
 	private int Head;
 	private int Staff_Vacancies;
 	
+	public DepartmentBean() {
+		
+	}
+	
+	public DepartmentBean(int Department_ID, int Hospital_ID, String Department_Name, int Head, int Staff_Vacancies) {
+		this.Department_ID = Department_ID;
+		this.Hospital_ID = Hospital_ID;
+		this.Department_Name = Department_Name;
+		this.Head = Head;
+		this.Staff_Vacancies = Staff_Vacancies;
+	}
+	
 	public int getDepartment_ID() {
 		return Department_ID;
 	}
