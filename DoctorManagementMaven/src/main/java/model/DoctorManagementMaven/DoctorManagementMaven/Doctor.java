58f5b0da825a365514619394d872509be1f72a26
chamathkaaa <uviniwijesinghe@gmail.com>
2020-04-17 02:28:45 +0530

    dbConnection

--- DoctorManagementMaven/src/main/java/model/DoctorManagementMaven/DoctorManagementMaven/Doctor.java
+++ DoctorManagementMaven/src/main/java/model/DoctorManagementMaven/DoctorManagementMaven/Doctor.java
@@ -1,5 +1,25 @@
 package model.DoctorManagementMaven.DoctorManagementMaven;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Doctor {
 
+	// A common method to connect to the DB
+		private Connection connect() {
+			Connection con = null;
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				// Provide the correct details: DBServer/DBName, username, password
+				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/healthcaredb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return con;
+		}	
+		
+		
+		
 }
