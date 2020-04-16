7bd55c9d151294874fd520402f36130032f4d1d6
chamathkaaa <uviniwijesinghe@gmail.com>
2020-04-16 14:07:20 +0530

    insertDoc in DocService

--- Doctor_Management_Service/src/com/DoctorService.java
+++ Doctor_Management_Service/src/com/DoctorService.java
@@ -22,10 +22,26 @@ public class DoctorService {
 	@GET
 	@Path("/")
 	@Produces(MediaType.TEXT_HTML)
-	public String readDoctors() {
+	public String readDoctors() 
+	{
 		//return "Hello";
 		return docObj.readDoctors();
 	}
 	
-	
+	@POST 
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String insertDoctor(@FormParam("DoctorName") String docName,       
+							@FormParam("NIC") String nic,    
+							@FormParam("Address") String address,       
+							@FormParam("MobileNo") String mobNo, 
+							@FormParam("Email") String email,
+							@FormParam("Specialization") String spec,
+							@FormParam("HospitalName") String hosp,
+							@FormParam("DepartmentName") String dept) 
+	{  
+		String output = docObj.insertDoctor(docName, nic, address, mobNo, email, spec, hosp, dept);  
+		return output; 
+	}
 }
