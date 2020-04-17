ac9374a82752a2e534b9bfa72273679bad7f594e
Prabha97 <prabhashwari.lk@gmail.com>
2020-04-17 16:03:27 +0530

    added new filter class

---
+++ Hospital_Management_Service/src/com/HospitalSecurity_Filter.java
@@ -0,0 +1,14 @@
+package com;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerRequestFilter;
+
+public class HospitalSecurity_Filter implements ContainerRequestFilter {
+
+	@Override
+	public ContainerRequest filter(ContainerRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
