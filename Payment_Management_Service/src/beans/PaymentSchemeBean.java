fc33076967e0542055a14cb4a61ee67bbc0ee83a
RPIUdishan <it18082234@my.sliit.lk>
2020-04-18 17:30:53 +0530

    complete payment scheme bean class

--- Payment_Management_Service/src/beans/PaymentSchemeBean.java
+++ Payment_Management_Service/src/beans/PaymentSchemeBean.java
@@ -1,7 +1,11 @@
 package beans;
-/*
-*PaymentSchemeBean class for payment schemes
-*/
+/**
+ * 
+ * @author Ishanka
+ * PaymentScheme class
+ * which describe the properties, getters and setters of a payment scheme
+ *
+ */
 public class PaymentSchemeBean {
 
 	private int id;
@@ -11,39 +15,99 @@ public class PaymentSchemeBean {
 	private double hosp_charge;
 	private double tax;
 	
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public double getTax() {
 		return tax;
 	}
+	/**
+	 * 
+	 * @param tax
+	 */
 	public void setTax(double tax) {
 		this.tax = tax;
 	}
+	
+	
+	/**
+	 * 
+	 * @return id
+	 */
 	public int getId() {
 		return id;
 	}
+	/**
+	 * 
+	 * @param id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	
+	/**
+	 * 
+	 * @return doc_id
+	 */
 	public int getDoc_id() {
 		return doc_id;
 	}
+	/**
+	 * 
+	 * @param doc_id
+	 */
 	public void setDoc_id(int doc_id) {
 		this.doc_id = doc_id;
 	}
+	
+	
+	/**
+	 * 
+	 * @return hospital_id
+	 */
 	public int getHospital_id() {
 		return hospital_id;
 	}
+	
+	/**
+	 * 
+	 * @param hospital_id
+	 */
 	public void setHospital_id(int hospital_id) {
 		this.hospital_id = hospital_id;
 	}
+	
+	
+	/**
+	 * 
+	 * @return doc_charge
+	 */
 	public double getDoc_charge() {
 		return doc_charge;
 	}
+	/**
+	 * 
+	 * @param doc_charge
+	 */
 	public void setDoc_charge(double doc_charge) {
 		this.doc_charge = doc_charge;
 	}
+	
+	
+	/**
+	 * 
+	 * @return hosp_charge
+	 */
 	public double getHosp_charge() {
 		return hosp_charge;
 	}
+	/**
+	 * 
+	 * @param hosp_charge
+	 */
 	public void setHosp_charge(double hosp_charge) {
 		this.hosp_charge = hosp_charge;
 	}
