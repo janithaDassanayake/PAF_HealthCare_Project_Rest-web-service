7a129f33f2bd5e5c1c6b7a2471023df2dd156b1c
janithaDassanayake <janithaprathapa@yahoo.com>
2020-04-18 10:49:50 +0530

    added new methord view schedule by day.

--- Appointment_Schedule_Service/src/beans/ScheduleBean.java
+++ Appointment_Schedule_Service/src/beans/ScheduleBean.java
@@ -13,7 +13,26 @@ public class ScheduleBean {
 	private int H_id;
 	private int App_id;
 	
+
+	public ScheduleBean() {
+		
+	}
 	
+	
+
+	public ScheduleBean(int schedule_id, String date, Time start_Time, Time end_Time, int d_id, int h_id, int app_id) {
+		Schedule_id = schedule_id;
+		Date = date;
+		Start_Time = start_Time;
+		End_Time = end_Time;
+		D_id = d_id;
+		H_id = h_id;
+		App_id = app_id;
+	}
+
+
+
+
 	public int getSchedule_id() {
 		return Schedule_id;
 	}
--- Appointment_Schedule_Service/src/com/Appointment_Schedule_Service.java
+++ Appointment_Schedule_Service/src/com/Appointment_Schedule_Service.java
@@ -2,171 +2,190 @@
 package com;
 
 import javax.ws.rs.Path;
-import java.sql.Date;
-import java.sql.Time;
+import javax.ws.rs.PathParam;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Timer;
-
+import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import beans.ScheduleBean;
 import model.AppoinmentSchedule;
-import model.AppoinmentType;
+
 
 @Path("/AppoinmentSchedule")
 public class Appointment_Schedule_Service {
 
 	AppoinmentSchedule aObj2 = new AppoinmentSchedule();
+
 	
 	// get all Schedules
-		@RolesAllowed({"admin","doctors","patient"})
-		@GET
-		@Path("/")
-		@Produces(MediaType.TEXT_HTML)
-		public String readAllSchedules() {
-			return aObj2.viewAllSchedule();
-		}
+	@RolesAllowed({ "admin", "doctors", "patient" })
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readAllSchedules() {
+		return aObj2.viewAllSchedule();
+	}
 
-		
-		
-		// add schedule
-		@RolesAllowed({"admin"})
-		@POST
-		@Path("/")
-		@Consumes(MediaType.APPLICATION_JSON)
-		@Produces(MediaType.TEXT_PLAIN)
-		public String enterSchedule(String scheduleData) {
-			String result = null;
-			// Convert the input string to a JSON object
-			JsonObject djosnObj = new JsonParser().parse(scheduleData).getAsJsonObject();
-
-			String appdate = djosnObj.get("Date").getAsString();
-			String appstart = djosnObj.get("Start_Time").getAsString();
-			String append = djosnObj.get("End_Time").getAsString();
-			int dId = djosnObj.get("D_id").getAsInt();
-			int hId = djosnObj.get("H_id").getAsInt();
-			int appId = djosnObj.get("App_id").getAsInt();
-
-			// ==================convert util time to sql time=====================
-			//DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-
-			DateFormat formatTime = new SimpleDateFormat("HH:mm:ss");
-
-			try {
-				//java.util.Date date = (java.util.Date) format.parse(appdate);
-				//java.sql.Date sDate = convertUtilToSql(date);
-
-				java.util.Date stime = (java.util.Date) formatTime.parse(appstart);
-				java.sql.Time startTime = convertUtilToSqlTime(stime);
-
-				java.util.Date etime = (java.util.Date) formatTime.parse(append);
-				java.sql.Time endTime = convertUtilToSqlTime(etime);
-
-				String output = aObj2.add_Appoinment_Schedule(appdate, startTime, endTime, dId, hId, appId);
-				result = output;
-
-			} catch (ParseException e) {
-				e.printStackTrace();
-				result = e.toString();
-			}
-			return result;
-
-		}
-		
-		
-		
 	
-		
-		// update Schedules
-		@RolesAllowed({"admin"})
-		@PUT
-		@Path("/")
-		@Consumes(MediaType.APPLICATION_JSON)
-		@Produces(MediaType.TEXT_PLAIN)
-		public String updateAppschedule(String TypeData) {
-			String result = null;
-			// Convert the input string to a JSON object
-			JsonObject sjosnObj = new JsonParser().parse(TypeData).getAsJsonObject();
+	
+	// View a schedule by given by day
 
-			// Read the values from the JSON object
-			String sId = sjosnObj.get("schedule_id").getAsString();
-			String appdate = sjosnObj.get("Date").getAsString();
-			String appstart = sjosnObj.get("Start_Time").getAsString();
-			String append = sjosnObj.get("End_Time").getAsString();
-			int dId = sjosnObj.get("D_id").getAsInt();
-			int hId = sjosnObj.get("H_id").getAsInt();
-			int appId = sjosnObj.get("App_id").getAsInt();
+	@RolesAllowed({ "admin", "doctors" })
+	@GET
+	@Path("/{Date}")
+	// @Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<ScheduleBean> readScheduleByGiven_DAY(@PathParam("Date") String date) {
+		List<ScheduleBean> list;
+		list = aObj2.View_Shedules_By_given_Day(date);
+		return list;
 
-			// ==================convert util time to sql time=====================
-			//DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+	}
+	
+	
+	
+	
+	// View a schedule by given by Schedule id
+	@RolesAllowed({"admin","doctors"})
+	@GET
+	@Path("/schedule/{schedule_id}")
+	// @Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public ScheduleBean readScheduleByGiven_ID(@PathParam("schedule_id") int S_id) {
+		return aObj2.Show_schedule_By_Id(S_id);
+	}
 
-			DateFormat formatTime = new SimpleDateFormat("HH:mm:ss");
+	
 
-			try {
-				//java.util.Date date = (java.util.Date) format.parse(appdate);
-				//java.sql.Date sDate = convertUtilToSql(date);
+	
+	
+	// add schedule
+	@RolesAllowed({ "admin" })
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String enterSchedule(String scheduleData) {
+		String result = null;
+		// Convert the input string to a JSON object
+		JsonObject djosnObj = new JsonParser().parse(scheduleData).getAsJsonObject();
 
-				java.util.Date stime = (java.util.Date) formatTime.parse(appstart);
-				java.sql.Time startTime = convertUtilToSqlTime(stime);
+		String appdate = djosnObj.get("Date").getAsString();
+		String appstart = djosnObj.get("Start_Time").getAsString();
+		String append = djosnObj.get("End_Time").getAsString();
+		int dId = djosnObj.get("D_id").getAsInt();
+		int hId = djosnObj.get("H_id").getAsInt();
+		int appId = djosnObj.get("App_id").getAsInt();
 
-				java.util.Date etime = (java.util.Date) formatTime.parse(append);
-				java.sql.Time endTime = convertUtilToSqlTime(etime);
+		// ==================convert util time to sql time=====================
+		// DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
-				String output = aObj2.updateAppointmentSchedule(Integer.parseInt(sId), appdate, startTime, endTime, dId, hId,appId);
-				result = output;
+		DateFormat formatTime = new SimpleDateFormat("HH:mm:ss");
 
-			} catch (ParseException e) {
-				e.printStackTrace();
-				result = e.toString();
-			}
-			return result;
+		try {
+			// java.util.Date date = (java.util.Date) format.parse(appdate);
+			// java.sql.Date sDate = convertUtilToSql(date);
 
+			java.util.Date stime = (java.util.Date) formatTime.parse(appstart);
+			java.sql.Time startTime = convertUtilToSqlTime(stime);
+
+			java.util.Date etime = (java.util.Date) formatTime.parse(append);
+			java.sql.Time endTime = convertUtilToSqlTime(etime);
+
+			String output = aObj2.add_Appoinment_Schedule(appdate, startTime, endTime, dId, hId, appId);
+			result = output;
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+			result = e.toString();
 		}
+		return result;
 
-		// =========************* return sql date and time*********==========
-		private static java.sql.Date convertUtilToSql(java.util.Date uDate) {
-			java.sql.Date sDate = new java.sql.Date(uDate.getTime());
-			return sDate;
+	}
+
+	
+	
+	// update Schedules
+	@RolesAllowed({ "admin" })
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateAppschedule(String TypeData) {
+		String result = null;
+		// Convert the input string to a JSON object
+		JsonObject sjosnObj = new JsonParser().parse(TypeData).getAsJsonObject();
+
+		// Read the values from the JSON object
+		String sId = sjosnObj.get("schedule_id").getAsString();
+		String appdate = sjosnObj.get("Date").getAsString();
+		String appstart = sjosnObj.get("Start_Time").getAsString();
+		String append = sjosnObj.get("End_Time").getAsString();
+		int dId = sjosnObj.get("D_id").getAsInt();
+		int hId = sjosnObj.get("H_id").getAsInt();
+		int appId = sjosnObj.get("App_id").getAsInt();
+
+		// ==================convert util time to sql time=====================
+		// DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+		DateFormat formatTime = new SimpleDateFormat("HH:mm:ss");
+
+		try {
+			// java.util.Date date = (java.util.Date) format.parse(appdate);
+			// java.sql.Date sDate = convertUtilToSql(date);
+
+			java.util.Date stime = (java.util.Date) formatTime.parse(appstart);
+			java.sql.Time startTime = convertUtilToSqlTime(stime);
+
+			java.util.Date etime = (java.util.Date) formatTime.parse(append);
+			java.sql.Time endTime = convertUtilToSqlTime(etime);
+
+			String output = aObj2.updateAppointmentSchedule(Integer.parseInt(sId), appdate, startTime, endTime, dId,
+					hId, appId);
+			result = output;
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+			result = e.toString();
 		}
+		return result;
 
-		private static java.sql.Time convertUtilToSqlTime(java.util.Date uDate) {
-			java.sql.Time sTime = new java.sql.Time(uDate.getTime());
-			return sTime;
-		}
+	}
 
-		
-		
-		// delete Schedules
-		@RolesAllowed({"admin"})
-		@DELETE
-		@Path("/")
-		@Consumes(MediaType.APPLICATION_JSON)
-		@Produces(MediaType.TEXT_PLAIN)
-		public String deleteAppointmentSchedule(String scheduleData) {
-			// Convert the input string to a JSON object
-			JsonObject doc = new JsonParser().parse(scheduleData).getAsJsonObject();
+	// =========************* return sql time*********==========
 
-			ScheduleBean appSchedule = new ScheduleBean();
-			// Read the value from the element <ID>
-			appSchedule.setSchedule_id(doc.get("schedule_id").getAsInt());
-			
-			String output = aObj2.deleteAppointmentSchedule(appSchedule);
-			return output;
-		}
-		
+	private static java.sql.Time convertUtilToSqlTime(java.util.Date uDate) {
+		java.sql.Time sTime = new java.sql.Time(uDate.getTime());
+		return sTime;
+	}
+
+	// delete Schedules
+	@RolesAllowed({ "admin" })
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteAppointmentSchedule(String scheduleData) {
+		// Convert the input string to a JSON object
+		JsonObject doc = new JsonParser().parse(scheduleData).getAsJsonObject();
+
+		ScheduleBean appSchedule = new ScheduleBean();
+		// Read the value from the element <ID>
+		appSchedule.setSchedule_id(doc.get("schedule_id").getAsInt());
+
+		String output = aObj2.deleteAppointmentSchedule(appSchedule);
+		return output;
+	}
+
 }
-
