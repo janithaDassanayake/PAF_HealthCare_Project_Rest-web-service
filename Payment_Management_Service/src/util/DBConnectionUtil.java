39016b9c5d697ae26ce0599db0ddda933282b336
RPIUdishan <it18082234@my.sliit.lk>
2020-04-18 17:33:09 +0530

    complete db connection class

--- Payment_Management_Service/src/util/DBConnectionUtil.java
+++ Payment_Management_Service/src/util/DBConnectionUtil.java
@@ -7,10 +7,15 @@ import java.sql.DriverManager;
  * 
  * @author Ishanka
  *get the DB connection here
+ *
  */
 
 public class DBConnectionUtil {
 	
+	/**
+	 * 
+	 * @return the Connection of database
+	 */
 	public Connection connect() {
 		
 		Connection conn = null;
@@ -20,19 +25,18 @@ public class DBConnectionUtil {
 
 			 Class.forName("com.mysql.jdbc.Driver"); 
 			 
-			 conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/payments", "root", "");
+			 conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/payments", "root", ""); //path for the database
 			
 			 
-			 //System.out.println("testing");
-			 if(conn == null) {
-				 System.out.println("Error with connection of database");
-			 }
-			 else{
-				 System.out.println("Successful connection of database");
-			 }
+			 
+			 if(conn == null) { System.out.println("Error with connection of database"); } //will be printed this in the console when db connection is fail
+			 
+			 else{ System.out.println("Successful connection of database"); } //will be printed this in the console when db connection is success
 
 		} catch (Exception e) {
+			
 			System.out.println("Connection Lost...");
+			
 			e.printStackTrace();
 		}
 		
