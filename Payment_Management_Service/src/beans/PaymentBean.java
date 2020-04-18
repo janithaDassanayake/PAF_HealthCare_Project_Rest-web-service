2e3f711fdb3bd9b47d756975837c3b2a905cfab1
RPIUdishan <it18082234@my.sliit.lk>
2020-04-18 17:30:25 +0530

    complete payment bean

--- Payment_Management_Service/src/beans/PaymentBean.java
+++ Payment_Management_Service/src/beans/PaymentBean.java
@@ -15,34 +15,69 @@ public class PaymentBean {
 	private double total_charge;
 
 	
-
+	/**
+	 * 
+	 * @return paymentScheme_id
+	 */
 	public int getPaymentScheme_id() {
 		return paymentScheme_id;
 	}
+	
+	/**
+	 * 
+	 * @param paymentScheme_id
+	 */
 	public void setPaymentScheme_id(int paymentScheme_id) {
 		this.paymentScheme_id = paymentScheme_id;
 	}
 	
 	
+	/**
+	 * 
+	 * @return payment_id
+	 */
 	public int getPayment_id() {
 		return payment_id;
 	}
+	
+	/**
+	 * 
+	 * @param payment_id
+	 */
 	public void setPayment_id(int payment_id) {
 		this.payment_id = payment_id;
 	}
 	
 	
+	/**
+	 * 
+	 * @return appointment_id
+	 */
 	public int getAppointment_id() {
 		return appointment_id;
 	}
+	
+	/**
+	 * 
+	 * @param appointment_id
+	 */
 	public void setAppointment_id(int appointment_id) {
 		this.appointment_id = appointment_id;
 	}
 	
 	
+	/**
+	 * 
+	 * @return total_charge
+	 */
 	public double getTotal_charge() {
 		return total_charge;
 	}
+	
+	/**
+	 * 
+	 * @param total_charge
+	 */
 	public void setTotal_charge(double total_charge) {
 		this.total_charge = total_charge;
 	}
